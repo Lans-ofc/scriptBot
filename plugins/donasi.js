ecf338f8b309e64f1fd6f714c46ae7c66c19da17
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2023-01-21 00:48:58 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
-    let img2 = `https://telegra.ph/file/a5eb5169ab1330a537e0b.jpg`
+    let img2 = `https://telegra.ph/file/3d89b5b51accafa08662c.jpg`
     return conn.sendButton(m.chat, 'Donasi Pm Owner aja.\n❏  :v', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
